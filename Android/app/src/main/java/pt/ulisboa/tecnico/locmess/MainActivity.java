51b25e88e906e7f31c1ce7b8f283f692675038a8
DiogoMRSilva <diogo.m.r.silva@ist.utl.pt>
2017-03-16 20:32:50 +0000

    preparing send message sreen

--- Android/app/src/main/java/pt/ulisboa/tecnico/locmess/MainActivity.java
+++ Android/app/src/main/java/pt/ulisboa/tecnico/locmess/MainActivity.java
@@ -1,10 +1,13 @@
 package pt.ulisboa.tecnico.locmess;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ListView;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -35,6 +38,20 @@ public class MainActivity extends ActivityWithDrawer{
         mRecyclerView.setAdapter(messagesAdapter);
 
 
+        FloatingActionButton myFab = (FloatingActionButton) findViewById(R.id.fabMain);
+        myFab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendMessage(v);
+            }
+        });
+
+
+
         super.onCreate(savedInstanceState);
+    }
+
+    protected void sendMessage(View v){
+        Intent intent = new Intent(this, PostMessageActivity.class);
+        startActivity(intent);
     }
 }
---
+++ Android/app/src/main/java/pt/ulisboa/tecnico/locmess/PostMessageActivity.java
@@ -0,0 +1,13 @@
+package pt.ulisboa.tecnico.locmess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_message);
+    }
+}
