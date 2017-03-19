a29768e2f369a3d237f824967dc1e45a24ef95d5
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2017-03-19 17:26:14 +0000

    Added log in screen

---
+++ Android/app/src/main/java/pt/ulisboa/tecnico/locmess/LoginActivity.java
@@ -0,0 +1,18 @@
+package pt.ulisboa.tecnico.locmess;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import static pt.ulisboa.tecnico.locmess.R.layout.activity_login;
+
+/**
+ * Created by nca on 19-03-2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_login);
+    }
+}
