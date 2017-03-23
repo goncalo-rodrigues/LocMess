0e0a4da26ada08842f8f543aa906dfd7a7cef9a6
DiogoMRSilva <diogo.m.r.silva@ist.utl.pt>
2017-03-23 13:58:03 +0000

    lalsa

---
+++ Lab2E1/app/src/main/java/pt/ulisboa/tecnico/cmov/lab2e1/MainActivity.java
@@ -0,0 +1,13 @@
+package pt.ulisboa.tecnico.cmov.lab2e1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
