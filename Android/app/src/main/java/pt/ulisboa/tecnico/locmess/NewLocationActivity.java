6d9b8c62d2aeb6df4f8120f6976f19221c103f35
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2017-03-25 15:14:44 +0000

    Fetching the near wireless networks

--- Android/app/src/main/java/pt/ulisboa/tecnico/locmess/NewLocationActivity.java
+++ Android/app/src/main/java/pt/ulisboa/tecnico/locmess/NewLocationActivity.java
@@ -2,24 +2,33 @@ package pt.ulisboa.tecnico.locmess;
 
 import android.Manifest;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -31,9 +40,18 @@ import static pt.ulisboa.tecnico.locmess.R.string.location;
 
 public class NewLocationActivity extends ActivityWithDrawer implements LocationListener {
     private static final int REQUEST_LOCATION = 0;
-    LocationManager mLocationManager;
-    double latitude = 100;
-    double longitude = 200;
+    private static final int REQUEST_WIFI_SCAN = 1;
+    private Context context = this;
+
+    private double latitude = 100;
+    private double longitude = 200;
+    private LocationManager mLocationManager;
+
+    private WifiManager wifi = null;
+    private List<ScanResult> scanRes;
+    private List<String> ssids = null;
+    private ArrayAdapter<String> arrayAdapter = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +59,7 @@ public class NewLocationActivity extends ActivityWithDrawer implements LocationL
         super.onCreate(savedInstanceState);
 
         mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
     }
 
     public void gpsClicked(View view) {
@@ -67,9 +86,63 @@ public class NewLocationActivity extends ActivityWithDrawer implements LocationL
 
         l = (LinearLayout) findViewById(R.id.chose_wifi);
         l.setVisibility(View.VISIBLE);
+
+        // Makes sure that WiFi is enabled
+        if (!wifi.isWifiEnabled()) {
+            Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+            startActivity(intent);
+        }
+
+        boolean permGranted =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_WIFI_STATE)
+                        == PackageManager.PERMISSION_GRANTED;
+
+        // Asks for permission
+        if(!permGranted)
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_WIFI_STATE},
+                    REQUEST_WIFI_SCAN);
+
+        // Receives WiFi scan results
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context c, Intent intent) {
+                scanRes = wifi.getScanResults();
+
+                if(ssids == null && scanRes.size() > 0) {
+                    ssids = new ArrayList<>();
+
+                    for (ScanResult sc : scanRes)
+                        ssids.add(sc.SSID);
+
+                    Collections.sort(ssids);
+                    ListView lv = (ListView) findViewById(R.id.wifi_ids_list);
+                    arrayAdapter = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, ssids);
+                    lv.setAdapter(arrayAdapter);
+                }
+            }
+        }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
+        checkGPSStatus();
+        wifi.startScan();
     }
 
     public void getCurrentLocation(View v) {
+        checkGPSStatus();
+
+        // Asks for the current location
+        try {
+        mLocationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 0, 0, this);
+        } catch(SecurityException e) {
+            // It should not happen
+        }
+
+        Location location = getLastKnownLocation();
+        onLocationChanged(location);
+    }
+
+    private void checkGPSStatus() {
         boolean permGranted =
                 ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                         == PackageManager.PERMISSION_GRANTED;
@@ -78,27 +151,14 @@ public class NewLocationActivity extends ActivityWithDrawer implements LocationL
         if(!permGranted) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    REQUEST_LOCATION);
-            return;
+                    REQUEST_WIFI_SCAN);
         }
-
-
-
-
 
         // Checks if the GPS is enabled
         if(!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
             startActivity(intent);
         }
-
-        // Asks for the current location
-        mLocationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER, 0, 0, this);
-
-        Location location = getLastKnownLocation();
-        onLocationChanged(location);
-        //TODO: This is not getting the gps coordinates!!! Please help Goncalo!!! #easteregg
     }
 
     public void getMapLocation(View v) {
@@ -123,7 +183,6 @@ public class NewLocationActivity extends ActivityWithDrawer implements LocationL
 
                     Location location = null;
 
-
                     location = getLastKnownLocation();
                     onLocationChanged(location);
 
@@ -133,7 +192,20 @@ public class NewLocationActivity extends ActivityWithDrawer implements LocationL
                         // It should not happen because permission was granted
                         e.printStackTrace();
                     }
+                }
+            }
 
+            case REQUEST_WIFI_SCAN: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // Receives WiFi scan results
+                    registerReceiver(new BroadcastReceiver() {
+                        @Override
+                        public void onReceive(Context c, Intent intent) {
+                            scanRes = wifi.getScanResults();
+                        }
+                    }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
                 }
             }
         }
