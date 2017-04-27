70caec61e63830c036859b4c525613eb52c75223
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2017-04-27 13:51:22 +0100

    Moving smartphone GPS

--- simulate_path.sh
+++ simulate_path.sh
@@ -1,11 +1,11 @@
 #http://latlongtrace.com/?path=m%60mkF%7Czwv@?iBb@%7B@j@aCp@uAZ%7DAGcEYgB?eBgBuAoE?eE%5DeFIsEGwEz@wCbAmDjBgB%5C?nB?v@q@f@I%60A
 
-
 rm -f script
-cat path.txt | while read line
+cat eul_run.kml | while read line
 do
-	lat=$(echo $(echo $line | grep -o '.*,' | sed 's/,//g;s/\./,/g'))
-	lon=$(echo $(echo $line | grep -o ',.*' | sed 's/,//g;s/\./,/g'))
+	coord=($(echo $line | grep -o -E "\-?[0-9]+\.[0-9]+"))
+	lat=$(echo ${coord[1]} | sed 's/\./,/g')
+	lon=$(echo ${coord[0]} | sed 's/\./,/g')
 	echo  "geo fix $lon $lat" >> script
    # do something with $line here
 done
