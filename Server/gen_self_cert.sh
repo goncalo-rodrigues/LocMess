de4497377d22bfe1cee16f57cf1d1c04e5d36d22
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2017-05-04 13:46:36 +0100

    Updated cert

--- Server/gen_self_cert.sh
+++ Server/gen_self_cert.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
 SUBJ="/CN=CMU/OU=DEI/O=locmess.duckdns.org/L=Lisbon/C=PT"
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -subj $SUBJ -nodes
+# -subj $SUBJ
+# Country Name (2 letter code) [AU]:PT
+# State or Province Name (full name) [Some-State]:Lisbon
+# Locality Name (eg, city) []:Lisbon
+# Organization Name (eg, company) [Internet Widgits Pty Ltd]:G02
+# Organizational Unit Name (eg, section) []:
+# Common Name (e.g. server FQDN or YOUR name) []:locmess.duckdns.org
+# Email Address []:
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes
