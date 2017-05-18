32ba5fd1c03da9b84c0db87a1836fda550440aba
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2017-05-18 13:47:00 +0100

    Increased the sleep time

--- simulate_path.sh
+++ simulate_path.sh
@@ -11,7 +11,7 @@ do
 	coord=($(echo $line | grep -o -E "\-?[0-9]+\.[0-9]+"))
 	lat=$(echo ${coord[1]} | sed 's/\./,/g')
 	lon=$(echo ${coord[0]} | sed 's/\./,/g')
-	sleep 0.1
+	sleep 2
 	echo  "geo fix $lon $lat"
    # do something with $line here
 done) | telnet localhost $port
